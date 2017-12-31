3f69879ff03e6393fc0df36b70c44182514b07ed
Sam Tarakajian <starakajian@gmail.com>
2017-12-31 09:01:35 -0600

    Adding pixi but for real

--- app/index.js
+++ app/index.js
@@ -5,6 +5,15 @@
 // Load application styles
 import 'styles/index.scss';
 
-// ================================
-// START YOUR APP HERE
-// ================================
+import * as PIXI from 'pixi.js';
+
+console.log("Okay, I guess this shit will work?");
+
+const app = new PIXI.Application({
+    width: 256,
+    height: 256
+});
+
+app.renderer.backgroundColor = 0x061639;
+
+document.body.appendChild(app.view);
